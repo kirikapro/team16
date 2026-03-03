d22ca836ccbc74688ef009a4b9d34aade3c8eacb
Akiho Ando <103188955+NQRSEA@users.noreply.github.com>
2026-03-03 11:25:59 +0000

    degab test

--- FeedTheFridge/SearchViewController.swift
+++ FeedTheFridge/SearchViewController.swift
@@ -8,22 +8,20 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        title = "Search"
 
-        // Do any additional setup after loading the view.
+        let label = UILabel()
+        label.text = "Search Screen"
+        label.font = .systemFont(ofSize: 24, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
