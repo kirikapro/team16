bdedd18127de3299ca23dc09af2d9d6a1c7b07d9
Akiho Ando <103188955+NQRSEA@users.noreply.github.com>
2026-03-03 12:02:32 +0000

    ignore

--- SceneDelegate.swift
+++ SceneDelegate.swift
@@ -12,16 +12,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-    func scene(
-        _ scene: UIScene,
-        willConnectTo session: UISceneSession,
-        options connectionOptions: UIScene.ConnectionOptions
-    ) {
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         let window = UIWindow(windowScene: windowScene)
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        window.rootViewController = storyboard.instantiateInitialViewController()
+
+        // ✅ テスト用：真っ赤な画面を表示
+        let vc = UIViewController()
+        vc.view.backgroundColor = .red
+        window.rootViewController = vc
 
         self.window = window
         window.makeKeyAndVisible()
