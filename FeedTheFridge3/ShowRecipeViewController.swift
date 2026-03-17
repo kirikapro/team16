4d83940aea5b25d84dacb63c9370815aedae5fda
michaglissmann <themichaglissmann@gmail.com>
2026-03-17 17:40:48 +0000

    ShowRecipe.2
    Reformatted labels on the ShowRecipeViewController so that it doesn't cut out the recipe name and made it so that the ingredients are seperated by a new line

--- FeedTheFridge3/ShowRecipeViewController.swift
+++ FeedTheFridge3/ShowRecipeViewController.swift
@@ -24,7 +24,7 @@ class ShowRecipeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         RecipeNameLabel.text = selectedRecipe.title
-        IngredientLabel.text = selectedRecipe.ingredients.joined(separator: ", ")
+        IngredientLabel.text = selectedRecipe.ingredients.joined(separator: "\n")
 
         // Do any additional setup after loading the view.
     }
